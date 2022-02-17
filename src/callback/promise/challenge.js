c3cc47851fe125bf007744d0b0ead781be0de850
Yorman Amaro <amarocarrero24@gmail.com>
2022-02-17 20:26:53 -0300

    Challenge-API-FetchDatacall to API

--- src/callback/promise/challenge.js
+++ src/callback/promise/challenge.js
@@ -1,25 +1,16 @@
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const fetchData = require('../utils/fetchData');
+const API = 'https://rickandmortyapi.com/api/character/';
 
-const fetchData = (url_api) => {
-    return new Promise((resolve, reject) => {
-      const xhttp = new XMLHttpRequest();
-      xhttp.open('GET', url_api, true);
-      xhttp.onreadystatechange = (() => {
-        if (xhttp.readyState === 4) {
-           (xhttp.status === 200)
-            ? resolve(JSON.parse(xhttp.responseText))  //  utilizando las collback para las logicas
-            : reject(new Error('Error', url_api))
-                
-
-
-
-                if (xhttp.status === 200) {
-                    Callback(null, JSON.parse(xhttp.responseText));    // utilizando promesas 
-                } else {
-                    const error = new Error('Error + url_api');
-                    return Callback(error, null)
-                }
-            }
-        }
+fetchData(API)
+    .then(data => {
+        console.log(data.info.count);
+        return fetchData(`${API}${data.results[0].id}`)
     })
-}
+    .then(data => {
+        console.log(data.name)
+        return fetchData(data.origin.url)
+    })
+    .then(data => {
+        console.log(data.dimension)
+    })
+    .catch(err => console.error(err));
